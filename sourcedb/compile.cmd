5dc887288900c6943dfa3737efaf8d9a6a8372e3
Siddharth Sharma <f20220601@pilani.bits-pilani.ac.in>
2024-07-03 18:31:15 +0530

    Update compile command

--- sourcedb/compile.cmd
+++ sourcedb/compile.cmd
@@ -1,2 +1,1 @@
-set JX_HOME=..\..
-javac -d .\bin -cp .;%JX_HOME%\libs\jxclasses.jar;%JX_HOME%\external_libs\json-20160212.jar @sources.txt
+javac -d .\bin -cp .;.\libs\jxclasses.jar;.\libs\json-20160212.jar @sources.txt
